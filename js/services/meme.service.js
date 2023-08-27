6dc1447603326b839d0b872b96f2bf99acaa27d2
yinonab <yinonabarjel@gmail.com>
2023-08-28 00:32:01 +0300

    border

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -136,7 +136,7 @@ function _createMems() {
                 {
                     x: 10,
                     y: 150,
-                    txt: '',
+                    txt: 'text',
                     size: gSize,
                     color: gColor,
                     font: gFont,
@@ -147,6 +147,21 @@ function _createMems() {
     }
     // _saveBookToStorage()
     return gMeme
+}
+function getEvPos(ev) {
+    let pos = {
+        x: ev.offsetX,
+        y: ev.offsetY,
+    }
+    if (TOUCH_EVS.includes(ev.type)) {
+        ev.preventDefault()
+        ev = ev.changedTouches[getSelectedLine()]
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+        }
+    }
+    return pos
 }
 function openGallery() {
     removeClass('hidden', 'images-container')
@@ -285,21 +300,7 @@ function reset() {
     removeClass('hidden', 'add')
     localStorage.clear()
 }
-function getEvPos(ev) {
-    let pos = {
-        x: ev.offsetX,
-        y: ev.offsetY,
-    }
-    if (TOUCH_EVS.includes(ev.type)) {
-        ev.preventDefault()
-        ev = ev.changedTouches[getSelectedLine()]
-        pos = {
-            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
-            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
-        }
-    }
-    return pos
-}
+
 function addListeners() {
     addMouseListeners()
     addTouchListeners()
